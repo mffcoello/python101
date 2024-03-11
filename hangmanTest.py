2aaa36a1da89130523e1ca075adc57e6bcffffae
Maria Coello <mayfco@icloud.com>
2024-03-11 01:56:59 -0400

    Add files via upload

--- hangmanTest.py
+++ hangmanTest.py
@@ -1,42 +1,114 @@
-#TOD0: account for letter repetition (need a for loop)
-#TODO account for reptitive tries
-#TODO change the print so it looks more like the game
+#TODO: 1st input not working/ not going into the list
+#TODO: get rid of duplicate on list (for loop)
 
 
 import random
 #word guesses/ list of words
 
-word_list = ["mayfness", "apple", "airplane","ashes" ]
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+word_list = [ "apple",
+    "river",
+    "chair",
+    "cloud",
+    "music",
+    "ocean",
+    "tiger",
+    "green",
+    "heart",
+    "smile",
+    "pizza",
+    "lemon",
+    "fairy",
+    "house",
+    "bread"]
 
 #call random word from list of words to check conditions
+MAX_WRONG = len(HANGMANPICS) - 1
 word = random.choice(word_list)
-guess = input("Guess a letter: ")
-correct_guess = len(word)
+correct_guess = 0
 print(word)
-user_guess = len(guess)
+used = []
+guess = input("Guess a letter: ")
+
+
 #check if the guess letter is in word x in wordlist
 
-while correct_guess != 0:
+while correct_guess < MAX_WRONG:
         if guess in word:
-            correct_guess -= 1
+
             print("Correct!")
+            print("You have used these letters", used)
             print(correct_guess)
-            guess = input("Guess a letter: ")
-        elif guess not in word:
             correct_guess = correct_guess
-            print("Incorrect!")
-            print(correct_guess)
             guess = input("Guess a letter: ")
-# condition if already tried this letter
+            used.append(guess)
+        elif guess not in word:
 
- # if guess == guess:
-# correct_guess = correct_guess
-# print("you already tried that!")
+            print(HANGMANPICS[correct_guess])
+            print("Incorrect!")
+            print("You have used these letters", used)
+            print(correct_guess)
+            correct_guess += 1
+            guess = input("Guess a letter: ")
+            used.append(guess)
 
+if correct_guess == MAX_WRONG:
+    print("You ded!")
 # Exit the game when  guesses <= 1
-        if correct_guess == 1:
-            print("You won!")
-            exit()
+if correct_guess == 1:
+    print("You won!")
+    exit()
+
 
 
 
